05e36d6388181cf64ba38bc3195ed483ff1a8194
Gustavo Hetrich <gustavohetrich@outlook.com>
2024-04-14 23:43:06 -0300

    Update Counter.py

--- Counter.py
+++ Counter.py
@@ -7,20 +7,30 @@ def count():
     given_name = counter_name.get()
     if not given_name.strip():
         no_text = ctk.CTkLabel(window, text = 'Please insert a Title')
-        no_text.grid(column = 0, sticky = 's')
+        no_text.grid(row = 0, column = 0, sticky = '')
         window.after(2000, no_text.destroy)
     else:
         print(given_name)
         # Name of the Counter
         date = datetime.now()
-        name_place = ctk.CTkLabel(title_place, text= given_name + "....." + date.strftime('%H:%M:%S'), font = ctk.CTkFont('roboto', size = 15))
+        name_place = ctk.CTkLabel(title_place, text = given_name + (' ' * 50)  + date.strftime('%H:%M:%S'), font = ctk.CTkFont('roboto', size = 20))
         name_place.pack(pady = 5)
         counter_name.delete(0, END)
+
+def changemode():
+    val = appearance.get()
+    if val:
+         ctk.set_appearance_mode('dark')
+    else:
+         ctk.set_appearance_mode('light')
+    
+
 
 
 def key_pressed(enter):
         count()
 
+# Window Configuration
 window = ctk.CTk()
 window.title('Counter')
 window.geometry('800x700')
@@ -32,8 +42,19 @@ window.columnconfigure(1, weight = 1)
 window.rowconfigure(0, weight = 1)
 
 #title
-title = ctk.CTkLabel(window, text= 'Counter', font = ctk.CTkFont('roboto', size = 50), fg_color = 'purple')
+title = ctk.CTkLabel(window, text= 'Counter', font = ctk.CTkFont('roboto', size = 40))
 title.grid(column = 0, sticky = 'nwe')
+
+
+# Dark and Light Mode
+appearance = ctk.CTkSwitch(window, text = 'Change Mode',
+font = ctk.CTkFont('roboto', size = 10),
+fg_color = 'purple',
+progress_color = 'orange',
+onvalue = 0,
+offvalue = 1,
+command = changemode,)
+appearance.grid(row = 0, column = 0, sticky = 's')
 
 
 #Input Field
